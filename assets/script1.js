24181cf465426e5c141a75c9c38a591b89e4de11
javana <venjavanapkk@gmail.com>
2024-05-09 23:57:35 +0630

    first commit

---
+++ assets/script1.js
@@ -0,0 +1,21 @@
+function switchOn() {
+    var light = document.getElementById("light");
+    light.style.backgroundImage = "url('./img/switch-on-png')"; // switch on
+  }
+  
+  function switchOff() {
+    var light = document.getElementById("light");
+    light.style.backgroundImage = "url('./img/switch-off.png')"; // switch off
+  };
+
+  function switchOn() {
+    var light = document.getElementById("light");
+    light.style.backgroundColor = "yellow"; // switch on
+  }
+  
+  function switchOff() {
+    var light = document.getElementById("light");
+    light.style.backgroundColor = "grey"; // switch off
+  }
+  
+  
